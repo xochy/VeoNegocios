e827cf322126ec26461a475707d7f7eb4858078f
xochy <xochydeather@gmail.com>
2020-03-24 21:32:10 -0600

    subida para heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('stores.index') }}">Negocios</a>
                         </li>
-                        <li class="nav-item">
+                        {{-- <li class="nav-item">
                             <a class="nav-link" href="{{ route('stores.showStoreCostumer', Auth::user()) }}">Mi Negocio</a>
-                        </li>
+                        </li> --}}
                         {{-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
